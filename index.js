f270cc0234059bbc950332d95179c0c1950ef82d
jeshurunmuchugingugi <jeshurun.muchugi@student.moringaschool.com>
2025-06-14 14:07:33 +0300

    Loops commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+
+function writeCards(names, eventName) {
+    let messages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+  
+    return messages;
+  }
+  writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+  
+  countDown(10);
